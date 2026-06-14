81b29a39191d900fd941cb49ebe2ca6348d96b3a
MimiX07 <mildredmarchan.mq@gmail.com>
2026-06-14 00:03:14 -0500

    feat: add profile selection and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Landing } from './features/auth/landing/landing';
+import { ProfileSelection } from './features/auth/profile-selection/profile-selection';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'auth', component: ProfileSelection },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/features/auth/profile-selection/profile-selection.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-selection',
+  imports: [RouterModule],
+  templateUrl: './profile-selection.html',
+  styleUrl: './profile-selection.css'
+})
+export class ProfileSelection {}
